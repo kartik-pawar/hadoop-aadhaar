08c68b0b93bd49b81254ab050dab2cf38bca2dfc
ishu-gunani0104 <ishu_gunani@persistent.com>
2018-01-20 13:13:25 +0530

    All queries done bro

--- Spark/try.py
+++ Spark/try.py
@@ -5,5 +5,6 @@ pairs = tF.map(lambda x:(x.split(",")[3],x.split(",")[6]))
 pairs = pairs.map(lambda x:(x,1))
 femaleP = pairs.filter(lambda x:x[0][1]=='F')
 femaleP = femaleP.groupByKey().mapValues(sum).collect()
-with open("sampleOutput2.csv","w") as fp:
-	fp.write('\n'.join('%s %s' % x for x in femaleP))
+femaleP = sorted(femaleP,key=lambda x:-x[1])[:10]
+with open("Female per district.csv","w") as fp:
+	fp.write('\n'.join('%s,%s' % x for x in femaleP))
